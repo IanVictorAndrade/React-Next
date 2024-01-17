2380095e5d8e3ba6c7adc541e34d23dfe402b67a
ianvictor <ianvictorandrade@hotmail.com>
2024-01-17 09:32:27 -0400

    usando state

--- src/components/hooks/ImagemAleatoria.tsx
+++ src/components/hooks/ImagemAleatoria.tsx
@@ -1,21 +1,20 @@
 import Image from "next/image";
+import { useState } from "react";
 
 export default function ImagemAleatoria() {
 
-    let pesquisa:string = '';
-    const url = 'https://source.unsplash.com/featured/300x300?'
-
-    function urlImagem() {
-        return `${url}${pesquisa}`
-    }
+    const [pesquisa, alterarPesquisa] = useState<string>('abstract')
+    const [tamanho, alterarTamanho] = useState<number>(300)
+    const url = 'https://source.unsplash.com/featured/'
 
     function renderizarBotao (valor: string) {
         return (
             <button className={`
                 bg-blue-500 text-white
                 px-4 py-2 rounded-md h-[100%] w-[100%]
-            `} onClick={() => {pesquisa = valor
-                console.log(urlImagem()); 
+            `} onClick={() => {
+                alterarPesquisa(valor);
+                console.log(valor); 
             }}>
                 {valor}
             </button>
@@ -24,13 +23,21 @@ export default function ImagemAleatoria() {
 
     return (
         <div className={`
-            border border-zinc-500 p-7 rounded-md
+            border border-zinc-500 p-7 rounded-md flex flex-col gap-3
         `}>
-            <Image src={urlImagem()} height={300} width={300} alt="Imagem" />
+            <Image src={`${url}${tamanho}x${tamanho}?${pesquisa}`} height={300} width={300} alt="Imagem" />
             <div className="flex justify-between gap-5">
                 {renderizarBotao('abstract')}
                 {renderizarBotao('city')}
                 {renderizarBotao('person')}
+            </div>
+            <div>
+                <input 
+                    type="number" 
+                    value={tamanho} 
+                    className="bg-zinc-800 p-2 rounded-md outline-none"
+                    onChange={e => alterarTamanho(+e.target.value)}
+                    />
             </div>
         </div>
     )
