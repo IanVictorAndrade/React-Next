4235dcd93c7d26a272e04f0805e5a04979cd1632
ianvictor <ianvictorandrade@hotmail.com>
2024-01-22 12:37:29 -0400

    wip

--- src/components/condicional/Faq.tsx
+++ src/components/condicional/Faq.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import Pergunta from "./Pergunta";
 
 export default function Faq() {
+    const [ativo, setAtivo] = useState<number>(0)
     return (
         <div className="flex flex-col gap-2 w-[90%] md:w-3/4">
             <Pergunta pergunta="oi, tudo bem?" resposta="simaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" />
--- src/components/condicional/Pergunta.tsx
+++ src/components/condicional/Pergunta.tsx
@@ -1,25 +1,24 @@
 import { useState } from "react"
 import If from "./If"
+import { IconChevronDown, IconChevronUp } from "@tabler/icons-react"
 
 interface PerguntaProps {
     pergunta: string
     resposta: string
+    aberta: boolean
+    alternarVisibilidade: () => void
 }
 
 export default function Pergunta(props: PerguntaProps) {
-    
-    const [aberta, setAberta] = useState<boolean>(false)
-
-
-    
     return (
         <div className={`
             border border-white rounded-md overflow-hidden
-        `} onClick={() => setAberta(!aberta)}>
-            <div className="bg-zinc-700 p-5 cursor-pointer select-none">
-                {props.pergunta}
+        `} onClick={() => props.alternarVisibilidade()}>
+            <div className="bg-zinc-700 p-5 cursor-pointer select-none flex justify-between">
+                <span>{props.pergunta}</span>
+                {props.aberta ? <IconChevronUp /> : <IconChevronDown />}
             </div>
-            <If teste={aberta} >
+            <If teste={props.aberta} >
                 <div className="p-5 select-none">
                     {props.resposta}
                 </div>
