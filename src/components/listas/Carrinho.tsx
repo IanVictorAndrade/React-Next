bd45295894296658f461642b2e3fdc80bba41384
ianvictor <ianvictorandrade@hotmail.com>
2024-01-24 12:13:00 -0400

    carrinho estilizado

--- src/components/listas/Carrinho.tsx
+++ src/components/listas/Carrinho.tsx
@@ -1,5 +1,6 @@
 import ItemCarrinho from "@/model/ItemCarrinho"
 import CarrinhoItem from "./CarrinhoItem"
+import { IconCircleX } from "@tabler/icons-react"
 
 interface CarrinhoProps {
     itens: ItemCarrinho[]
@@ -9,12 +10,24 @@ interface CarrinhoProps {
 
 export default function Carrinho(props: CarrinhoProps) {
     return (
-        <div>
-            {props.itens.map((item, i) => {
-                return (
-                    <CarrinhoItem key={i} {...item} />
-                )
-            })}
+        <div className="flex flex-col border border-white rounded-md overflow-hidden w-4/5">
+            <div className="bg-zinc-800 text-3xl p-3">
+                <span>Carrinho</span>
+            </div>
+            <div className="flex gap-5  p-5">
+                {props.itens.length === 0 ? (
+                    <div className="flex gap-3 justify-center text-zinc-500">
+                        <IconCircleX />
+                        <span>Nenhum item no carrinho</span>
+                    </div>
+                ): (
+                    props.itens.map((item, i) => {
+                        return (
+                            <CarrinhoItem key={i} {...item} />
+                        )
+                    })
+                )}
+            </div>
         </div>
     )
 }
